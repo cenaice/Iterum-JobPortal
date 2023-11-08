86e5176ee6258bb478c54d11a0d377e221e66eb5
Victer <victerphiathep@gmail.com>
2023-11-07 19:36:13 -0500

    Fixed font size issues

--- iterum-job-portal/src/App.js
+++ iterum-job-portal/src/App.js
@@ -4,26 +4,31 @@ import { Navbar } from "./components/Navbar";
 import { Hero } from "./pages/Hero";
 import { FeaturesCards } from "./components/FeaturesCards";
 import { JobPortal } from "./pages/JobPortal";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';  // Import necessary routing components
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ProfilePage } from "./pages/ProfilePage";
 import { About } from "./pages/About";
 
 export default function App() {
   return (
-    <MantineProvider defaultColorScheme="light">
-      <Router>
-        <Navbar />
-        <div>
-          {/* Set up the Routes */}
+    <>
+      <MantineProvider
+        theme={{
+          fontFamily: 'Verdana, sans-serif',
+          fontFamilyMonospace: 'Monaco, Courier, monospace',
+          headings: { fontFamily: 'Greycliff CF, sans-serif' },
+        }}
+      >
+        <Router>
+          <Navbar />
           <Routes>
             <Route path="/" element={<><Hero /><FeaturesCards /></>} />
             <Route path="/jobportal" element={<JobPortal />} />
             <Route path="/profilepage" element={<ProfilePage />} />
             <Route path="/aboutpage" element={<About />} />
           </Routes>
-        </div>
-      </Router>
-    </MantineProvider>
+        </Router>
+      </MantineProvider>
+    </>
   );
 }
 
--- iterum-job-portal/src/components/JobPortalComponents/JobTable.jsx
+++ iterum-job-portal/src/components/JobPortalComponents/JobTable.jsx
@@ -1,57 +1,29 @@
 import { Table, Progress, Anchor, Text, Group } from '@mantine/core';
 import classes from './JobTable.module.css';
+import React, { useState, useEffect } from 'react';
+import { database } from '../../firebase/firebase.js';
+import { ref, onValue } from "firebase/database"
 
-const data = [
-  {
-    apply_link: 'Foundation',
-    company_name: 'Palantir',
-    date_posted: 1951,
-    location: { positive: 2223, negative: 259 },
-    role: "Morning Star Development Program",
-  },
-  {
-    apply_link: 'Frankenstein',
-    company_name: 'Mary Shelley',
-    date_posted: 1818,
-    location: { positive: 5677, negative: 1265 },
-    role: "Lorem Ipsum Co."
-  },
-  {
-    apply_link: 'Solaris',
-    company_name: 'Stanislaw Lem',
-    date_posted: 1961,
-    location: { positive: 3487, negative: 1845 },
-    role: "Lorem Ipsum University"
-  },
-  {
-    apply_link: 'Dune',
-    company_name: 'Frank Herbert',
-    date_posted: 1965,
-    location: { positive: 8576, negative: 663 },
-    role: "Lorem Ipsum Store"
-  },
-  {
-    apply_link: 'The Left Hand of Darkness',
-    company_name: 'Ursula K. Le Guin',
-    date_posted: 1969,
-    location: { positive: 6631, negative: 993 },
-    role: "Lorem Ipsum Solar"
-  },
-  {
-    apply_link: 'A Scanner Darkly',
-    company_name: 'Philip K Dick',
-    date_posted: 1977,
-    location: { positive: 8124, negative: 1847 },
-    role: "Lorem Ipsum Ipsum Lorem"
-  },
-];
+const data = [];
 
 export function JobTable() {
-  const rows = data.map((row) => {
-    const totalReviews = row.location.negative + row.location.positive;
-    const positiveReviews = (row.location.positive / totalReviews) * 100;
-    const negativeReviews = (row.location.negative / totalReviews) * 100;
 
+  const [data, setData] = useState([]);
+
+
+  useEffect(() => {
+    const databaseRef = ref(database, 'JobPostings');
+    onValue(databaseRef, (snapshot) => {
+      const dbData = snapshot.val();
+      const dataArray = Object.keys(dbData).map((key) => dbData[key]);
+      setData(dataArray);
+    });
+  }, []);
+
+
+
+
+  const rows = data.map((row) => {
     return (
       <Table.Tr key={row.company_name}>
         <Table.Td>
@@ -62,32 +34,12 @@ export function JobTable() {
         <Table.Td>{row.date_posted}</Table.Td>
         <Table.Td>
           <Anchor component="button" fz="sm">
-            {row.date_posted}
+            {row.role}
           </Anchor>
         </Table.Td>
-        <Table.Td>{Intl.NumberFormat().format(totalReviews)}</Table.Td>
+        <Table.Td>{row.location}</Table.Td>
         <Table.Td>
-          <Group justify="space-between">
-            <Text fz="xs" c="teal" fw={700}>
-              {positiveReviews.toFixed(0)}%
-            </Text>
-            <Text fz="xs" c="red" fw={700}>
-              {negativeReviews.toFixed(0)}%
-            </Text>
-          </Group>
-          <Progress.Root>
-            <Progress.Section
-              className={classes.progressSection}
-              value={positiveReviews}
-              color="teal"
-            />
-
-            <Progress.Section
-              className={classes.progressSection}
-              value={negativeReviews}
-              color="red"
-            />
-          </Progress.Root>
+          Button
         </Table.Td>
       </Table.Tr>
     );
@@ -101,8 +53,8 @@ export function JobTable() {
             <Table.Th>Company</Table.Th>
             <Table.Th>Date Posted</Table.Th>
             <Table.Th>Position</Table.Th>
-            <Table.Th>Reviews</Table.Th>
-            <Table.Th>Reviews distribution</Table.Th>
+            <Table.Th>Location</Table.Th>
+            <Table.Th>Apply</Table.Th>
           </Table.Tr>
         </Table.Thead>
         <Table.Tbody>{rows}</Table.Tbody>
