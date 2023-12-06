1a618686271ba5a3a93ceaca81773a37aede7dfa
Victer <victerphiathep@gmail.com>
2023-12-06 13:44:24 -0500

    AI/ML

--- iterum-job-portal/src/AI_ML/.ipynb_checkpoints/model_training-checkpoint.py
+++ iterum-job-portal/src/AI_ML/.ipynb_checkpoints/model_training-checkpoint.py
@@ -15,7 +15,7 @@ file_path_dice = '../../../datasets/dice_com-job_us_sample.csv'
 dice_data = pd.read_csv(file_path_dice)
 
 # Randomly sample a subset of the data (100% in this case)
-sampled_data = dice_data.sample(frac=0.75, random_state=1)
+sampled_data = dice_data.sample(frac=1, random_state=1)
 
 # Remove duplicates
 sampled_data.drop_duplicates(inplace=True)
@@ -25,17 +25,14 @@ sampled_data.fillna('unknown', inplace=True)
 
 # Select relevant columns and preprocess text
 # Update these column names based on the actual columns in your dataset, change according to dataset
-sampled_data = sampled_data[[
-    'jobtitle', 'jobdescription', 'employmenttype_jobstatus', 'skills']]
+sampled_data = sampled_data[['jobtitle', 'jobdescription', 'employmenttype_jobstatus', 'skills']]
 
 # Apply preprocessing to text columns
 for column in ['jobtitle', 'jobdescription', 'employmenttype_jobstatus', 'skills']:
     sampled_data[column] = sampled_data[column].apply(preprocess_text)
 
 # Concatenate text features into a single feature for analysis
-sampled_data['combined_text'] = sampled_data['jobtitle'] + ' ' + sampled_data['jobdescription'] + \
-    ' ' + sampled_data['employmenttype_jobstatus'] + \
-    ' ' + sampled_data['skills']
+sampled_data['combined_text'] = sampled_data['jobtitle'] + ' ' + sampled_data['jobdescription'] + ' ' + sampled_data['employmenttype_jobstatus'] + ' ' + sampled_data['skills']
 
 # Apply the function to create the target variable
 sampled_data['is_front_end_dev'] = sampled_data.apply(
@@ -46,7 +43,6 @@ sampled_data['is_back_end_dev'] = sampled_data.apply(
 
 sampled_data['is_ai_ml'] = sampled_data.apply(
     lambda x: JobCategoryClassifier.is_ai_ml(x['jobtitle'], x['jobdescription']), axis=1)
-
 
 def training_model(X, y, class_name):
     """
@@ -64,8 +60,7 @@ def training_model(X, y, class_name):
     """
 
     # Split the data into training and test sets
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.8, random_state=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8, random_state=1)
 
     # Create a pipeline that transforms the data using TF-IDF and then fits the Naive Bayes classifier
     model = make_pipeline(TfidfVectorizer(), MultinomialNB())
@@ -73,22 +68,48 @@ def training_model(X, y, class_name):
     # Train the model
     model.fit(X_train, y_train)
 
-    # Extract the trained vectorizer
+    print(f"Accuracy of {class_name}:", accuracy_score(y_test, model.predict(X_test)))
+
+    return model, X_test, y_test
+
+
+
+front_end_model, X_test_fe, y_test_fe = training_model(sampled_data['combined_text'], sampled_data['is_front_end_dev'], "Front End Dev")
+
+back_end_model, X_test_be, y_test_be = training_model(sampled_data['combined_text'], sampled_data['is_back_end_dev'], "Back End Dev")
+
+ai_ml_model, X_test_ai, y_test_ai = training_model(sampled_data['combined_text'], sampled_data['is_ai_ml'], "AI ML")
+
+
+def shap_explainability(X, model):
     vectorizer = model.named_steps['tfidfvectorizer']
+    classifier = model.named_steps['multinomialnb']
+    X_transformed = vectorizer.transform(X)
 
-    print(f"Accuracy of {class_name}:", accuracy_score(
-        y_test, model.predict(X_test)))
+    explainer = shap.Explainer(classifier, X_transformed)
+    shap_values = explainer(X_transformed)
 
-    return model, vectorizer, X_test, y_test
+    # Visualize the SHAP values for a specific prediction
+    shap.initjs()
+    return shap.force_plot(explainer.expected_value[0], shap_values[0][0], feature_names=vectorizer.get_feature_names_out())
+
+# Example usage
+#shap_plot = shap_explainability(X_test_fe, front_end_model)
+
+    
+def predict_proba_wrapper(X):
+    # Make sure it returns [n_samples, 2] shaped array
+    return front_end_model.predict_proba(X)
+
+# Use this wrapper in LIME
+def LIME_explainability(X_test, model):
+    explainer = LimeTextExplainer(class_names=['Not ', 'Is '])
+    idx = 1  # Make sure this index is valid in X_test
+    exp = explainer.explain_instance(X_test[idx], predict_proba_wrapper, num_features=10)
+    exp.show_in_notebook()
+    print(exp.as_list())
 
 
-front_end_model, vectorizer_fe, X_test_fe, y_test_fe = training_model(
-    sampled_data['combined_text'], sampled_data['is_front_end_dev'], "Front End Dev")
-
-# back_end_model, X_test_be, y_test_be = training_model(sampled_data['combined_text'], sampled_data['is_back_end_dev'], "Back End Dev")
-
-# ai_ml_model, X_test_ai, y_test_ai = training_model(sampled_data['combined_text'], sampled_data['is_ai_ml'], "AI ML")
-
-
-import eli5
-from sklearn.linear_model import LogisticRegression
+LIME_explainability(X_test_fe, front_end_model)
+LIME_explainability(X_test_be, back_end_model)
+LIME_explainability(X_test_ai, ai_ml_model)
